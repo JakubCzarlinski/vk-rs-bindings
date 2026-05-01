0fe3731e0344be5690e7ab7a508b063aa4097dbe
Jakub Czarlinski <Jakub.Czarlinski@Warwick.ac.uk>
2026-05-01 12:53:57 +0100

    Replace old tests with golden values via diff.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
+mkdir -p registry
+curl -o registry/video.xml https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/refs/heads/main/xml/video.xml
+curl -o registry/vk.xml https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/refs/heads/main/xml/vk.xml
+
 cargo run -p vk-codegen -- \
-  --vk ./vk-codegen/tests/fixtures/vk.xml \
-  --video ./vk-codegen/tests/fixtures/video.xml \
+  --vk ./registry/vk.xml \
+  --video ./registry/video.xml \
   --out ./vk-rs-bindings
 
 cargo fmt && cargo clippy --all-features --fix --allow-dirty && cargo fmt
